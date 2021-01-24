347b1563cddb23f30f026747d711e79c4e2da848
Santiago Mónaco <santiago.monaco33@gmail.com>
2021-01-24 14:26:42 -0300

    Corregido Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,12 @@ import React from "react";
 const Footer = () => {
   const fecha = new Date().getFullYear();
   return (
-      <div>
-        <footer className="ed-container">
-          <p className="ed-item s-center">
-            Todos los derechos reservados &copy; {fecha}
-          </p>
-        </footer>
-      </div>
-    );
+    <footer className="ed-container">
+      <p className="ed-item s-center">
+        Todos los derechos reservados &copy; {fecha}
+      </p>
+    </footer>
+   );
 };
 
 export default Footer;
