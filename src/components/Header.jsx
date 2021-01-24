aff36cd6a2f88ac5de3941028729a057c97f6d74
Santiago Mónaco <santiago.monaco33@gmail.com>
2021-01-24 14:21:44 -0300

    Actualizando vista Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Header = ({ title }) => {
-  return <h2>{title}</h2>;
-};
+const Header = ({ title }) => (
+    <header className="ed-container">
+      <div className="ed-item logo">
+        <h2>{title}</h2>
+      </div>
+    </header>
+  );
 
 export default Header;
